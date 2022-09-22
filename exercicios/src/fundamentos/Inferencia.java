34cbdbcc9f06ae32121879577b2b7f3e1c690a35
wellersonmonteiro <wellersonjava@gmail.com>
2022-09-22 16:25:14 -0300

    novas atividas

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		double d; // variável foi declarada 
+		d = 123.33; // variável foi inicializada 
+		System.out.println(d); // variável foi usada
+		
+	}
+
+}
